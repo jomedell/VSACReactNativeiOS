840fa909dedc05c812c4ad19e9237134ea02c75a
jomedell <42381371+jomedell@users.noreply.github.com>
2018-12-05 11:07:33 -0600

    Add files via upload

---
+++ appcenter-post-build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#  appcenter-post-build.sh
+#  VSAC
+#
+#  Created by Jorge Medellin on 4/23/18.
+#  Copyright © 2018 Microsoft. All rights reserved.
+
+#!/usr/bin/env bash
+#
+# For Xamarin, run all NUnit test projects that have "Test" in the name.
+# The script will build, run and display the results in the build logs.
+
+
+set -e
+
+echo "******************************************************"
+echo "Post Build Script"
+echo "******************************************************"
+
+
+
+echo "*******************************"
+echo "Post Build Script Complete"
+echo "*******************************"
+
---
+++ appcenter-pre-build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#  appcenter-pre-build.sh
+#  VSAC
+#
+#  Created by Jorge Medellin on 4/23/18.
+#  Copyright © 2018 Microsoft. All rights reserved.
+
+#!/usr/bin/env bash
+#
+# For Xamarin, run all NUnit test projects that have "Test" in the name.
+# The script will build, run and display the results in the build logs.
+
+
+set -e
+
+echo "******************************************************"
+echo "Pre Build Script"
+echo "******************************************************"
+
+
+
+echo "*******************************"
+echo "Pre Build Script Complete"
+echo "*******************************"
