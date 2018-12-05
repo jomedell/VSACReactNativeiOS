7c613c1d66c68d3773e55d8780d2ba4b5c6d4dda
jomedell <42381371+jomedell@users.noreply.github.com>
2018-12-05 11:26:20 -0600

    Add files via upload

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#  appcenter-post-clone.sh
+#  VSAC
+#
+#  Created by Jorge Medellin on 4/23/18.
+#  Copyright © 2018 Microsoft. All rights reserved.
+
+#!/usr/bin/env bash
+#
+# For Xamarin, run all NUnit test projects that have "Test" in the name.
+# The script will build, run and display the results in the build logs.
+
+
+set -e
+
+echo "******************************************************"
+echo "Post Clone Script"
+echo "******************************************************"
+
+
+
+echo "*******************************"
+echo "Post Clone Script Complete"
+echo "*******************************"
