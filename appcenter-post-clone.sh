0b8e409a4dbe54eeb79f7b558e7ebc6df2e17ca6
jomedell <42381371+jomedell@users.noreply.github.com>
2018-12-05 13:43:53 -0600

    Add files via upload

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
 # Install the proper node version
-
-
-
-
 set -ex
 brew uninstall node@6
 NODE_VERSION="11.3.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
-
-
-
-
